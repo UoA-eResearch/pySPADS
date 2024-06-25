5562fab11d5f8376bfb080896343bc4580be706a
Nelis Drost <nelis.drost@gmail.com>
2024-06-25 15:48:30 +1200

    feat: implement intercept fitting with mreg2

--- optimization/__init__.py
+++ optimization/__init__.py
@@ -5,24 +5,30 @@ from sklearn.linear_model import LinearRegression
 # Recreation of linear regression method in matlab code - appears to be less accurate than scikitlearns implementation
 
 
-def calc_SSE_all_Coef(theta: np.ndarray, S, PC):
+def calc_SSE_all_Coef(theta: np.ndarray, S, PC, fit_intercept=False):
     m = np.zeros((1, PC.shape[0]))
     for j in range(PC.shape[1]):
         X1 = theta[j] * PC[:, j]
         m += X1
+    if fit_intercept:
+        m += theta[-1]
 
     d1 = np.sum((S - m) ** 2)
     d2 = np.var(S) + np.var(m) + (np.mean(S) - np.mean(m)) ** 2
     return -(1 - d1 / (d2 * len(S)))
 
 
-def mreg2(y, X):
-    # TODO: use intercept
-    reg = LinearRegression().fit(X, y)
+def mreg2(y, X, fit_intercept=False):
+    reg = LinearRegression(fit_intercept=fit_intercept).fit(X, y)
     bb = reg.coef_
+    if fit_intercept:
+        bb = np.append(bb, reg.intercept_)
 
-    beta = fmin(calc_SSE_all_Coef, bb, args=(y.to_numpy(), X.to_numpy()),
+    beta = fmin(calc_SSE_all_Coef, bb, args=(y.to_numpy(), X.to_numpy(), fit_intercept),
                 maxiter=100000, maxfun=100000,
                 disp=False)
 
-    return beta
+    if fit_intercept:
+        return beta[:-1], beta[-1]
+    else:
+        return beta, 0
---
+++ tests/test_mreg.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+from hypothesis import given, settings, assume
+from hypothesis.strategies import floats, lists, composite
+from pytest import skip
+
+from optimization import mreg2
+from sklearn.linear_model import LinearRegression, Ridge
+
+@composite
+def coeffs_signals_drivers(draw, include_intercept=False, max_coeff=1000):
+    """
+    Generate random linear coefficients (+ optional intercept), then generate random driver data and a signal using
+    those coefficients
+    """
+    sensible_floats = (
+        floats(allow_nan=False, allow_infinity=False, allow_subnormal=False,
+               min_value=-max_coeff, max_value=max_coeff)
+        .filter(lambda x: x == 0 or np.abs(x) > 1e-4)
+    )
+
+    coeffs = draw(lists(sensible_floats, min_size=1, max_size=10))
+
+    if include_intercept:
+        intercept = draw(sensible_floats)
+    else:
+        intercept = 0.0
+
+    num_drivers = len(coeffs)
+    num_samples = 1000
+
+    drivers = {f'driver_{i}': np.random.randn(num_samples) for i in range(num_drivers)}
+    drivers_df = pd.DataFrame(drivers)
+
+    signal = intercept + np.sum([drivers[f'driver_{i}'] * coeffs[i] for i in range(num_drivers)], axis=0)
+    signal_sr = pd.Series(signal)
+
+    return (intercept, coeffs, drivers_df, signal_sr)
+
+
+@given(coeffs_signals_drivers(include_intercept=False))
+def test_mreg_no_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+    assert intercept == 0, "Not expecting to use intercept"
+
+    _coeffs, _ = mreg2(signal_sr, drivers_df, fit_intercept=False)
+
+    # Check the coefficients are close
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
+
+
+@skip("mreg does not work well with intercept")
+@given(coeffs_signals_drivers(include_intercept=True))
+def test_mreg_with_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+
+    _coeffs, _intercept = mreg2(signal_sr, drivers_df, fit_intercept=True)
+
+    # Check the coefficients are close
+    assert np.isclose(_intercept, intercept, rtol=1e-1, atol=1e-1)
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
+
+
+@given(coeffs_signals_drivers(include_intercept=False))
+def test_linreg_no_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+    assert intercept == 0, "Not expecting to use intercept"
+
+    reg = LinearRegression(fit_intercept=False).fit(drivers_df, signal_sr)
+    _coeffs = reg.coef_
+
+    # Check the coefficients are close
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
+
+
+@given(coeffs_signals_drivers(include_intercept=True))
+def test_linreg_with_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+
+    reg = LinearRegression(fit_intercept=True).fit(drivers_df, signal_sr)
+    _intercept = reg.intercept_
+    _coeffs = reg.coef_
+
+    # Check the coefficients are close
+    assert np.isclose(_intercept, intercept, rtol=1e-1, atol=1e-1)
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
+
+
+@given(coeffs_signals_drivers(include_intercept=False, max_coeff=50))
+def test_ridge_no_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+    assert intercept == 0, "Not expecting to use intercept"
+
+    reg = Ridge(fit_intercept=False).fit(drivers_df, signal_sr)
+    _coeffs = reg.coef_
+
+    # Check the coefficients are close
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
+
+
+@given(coeffs_signals_drivers(include_intercept=True, max_coeff=50))
+def test_ridge_with_intercept(test_data):
+    intercept, coeffs, drivers_df, signal_sr = test_data
+
+    reg = Ridge(fit_intercept=True).fit(drivers_df, signal_sr)
+    _intercept = reg.intercept_
+    _coeffs = reg.coef_
+
+    # Check the coefficients are close
+    assert np.isclose(_intercept, intercept, rtol=1e-1, atol=1e-1)
+    for i in range(len(coeffs)):
+        assert np.isclose(_coeffs[i], coeffs[i], rtol=1e-1, atol=1e-1)
