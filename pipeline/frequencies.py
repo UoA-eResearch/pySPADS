6f9ec64cd6b9ffd1b60f969100a4560bf8237430
Nelis Drost <nelis.drost@gmail.com>
2024-07-30 13:49:10 +1200

    refactor: replace prints in frequencies.py with logger.info

--- pipeline/frequencies.py
+++ pipeline/frequencies.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+import logging
 
 from processing.recomposition import component_frequencies, nearest_frequency, relative_frequency_difference
+
+logger = logging.getLogger(__name__)
 
 
 def match_frequencies(imfs: dict[str, pd.DataFrame], signal: str, threshold: float) -> pd.DataFrame:
@@ -13,18 +16,18 @@ def match_frequencies(imfs: dict[str, pd.DataFrame], signal: str, threshold: flo
     :return: DataFrame of nearest input IMF mode for each output IMF mode, with one column for each input time series
     """
     # Get the frequency of each component of each imf
-    print('Calculating IMF mode frequencies')
+    logger.info('Calculating IMF mode frequencies')
     max_mode = max([imf_df.shape[1] for imf_df in imfs.values()])
     freq_df = pd.DataFrame(index=range(max_mode), columns=list(imfs.keys()))
     for label, imf_df in imfs.items():
         freq_df[label] = component_frequencies(imf_df)
 
     # Find the nearest frequency in each input IMF to the frequency of each output mode
-    print('Matching signal modes to nearest frequencies in input modes')
+    logger.info('Matching signal modes to nearest frequencies in input modes')
     nearest_freq = nearest_frequency(freq_df[signal], freq_df.drop(columns=[signal]))
 
     # Check if the nearest frequency is within the threshold
-    print('Checking if nearest frequencies are within threshold')
+    logger.info('Checking if nearest frequencies are within threshold')
     rel_diff_df = relative_frequency_difference(freq_df[signal], freq_df.drop(columns=[signal]), nearest_freq)
 
     valid_components = (rel_diff_df < threshold).sum(axis=1)
@@ -33,12 +36,12 @@ def match_frequencies(imfs: dict[str, pd.DataFrame], signal: str, threshold: flo
                          f'{valid_components[valid_components == 0].index}')
 
     if any(valid_components < 3):
-        print(f'Warning: some output components have less than 3 valid input components: '
+        logger.info(f'Warning: some output components have less than 3 valid input components: '
               f'{valid_components[valid_components < 3].index}')
 
     # Show the components which are used for each output component
-    print('Components used for each output component:')
+    logger.info('Components used for each output component:')
     for i in rel_diff_df.index:
-        print(f'{i:>5} : {np.sum(rel_diff_df.loc[i] < threshold)}')
+        logger.info(f'{i:>5} : {np.sum(rel_diff_df.loc[i] < threshold)}')
 
     return nearest_freq[rel_diff_df < threshold]
