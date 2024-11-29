977d419e316f3e3b25ac9d8796e4cc1a7fc4e0fe
Nelis Drost <nelis.drost@gmail.com>
2024-11-29 16:34:16 +1300

    feat(cli): complete and document decompose command

--- src/pySPADS/cli.py
+++ src/pySPADS/cli.py
@@ -20,22 +20,15 @@ def cli():
 
 
 @cli.command()
-@click.option(
-    "-i",
-    "--input",
-    type=click.Path(exists=True, file_okay=True, dir_okay=True, path_type=pathlib.Path),
-    help="Input data file or directory, expects either a .csv, or a directory containing .csvs",
-    required=True,
-)
+@click.argument("files", type=click.Path(exists=True, file_okay=True, dir_okay=True, path_type=pathlib.Path), nargs=-1)
 @click.option(
     "-o",
     "--output",
     type=click.Path(
         exists=False, file_okay=False, dir_okay=True, path_type=pathlib.Path
     ),
-    help="Output directory",
+    help="Output directory. Defaults to current working directory. It is highly recommended that this is a separate directory to that containing the input files",
 )
-@click.option("-s", "--signal", type=str, help="Column name of signal to fit to")
 @click.option("--timecol", type=str, default="t", help="Column name of time index")
 @click.option(
     "-n",
@@ -43,37 +36,64 @@ def cli():
     cls=OptionNargs,
     type=tuple[float],
     callback=parse_noise_args,
-    help="Noise values to use when decomposing IMFs",
+    help="Noise values to use when decomposing IMFs, e.g. -n 0.1 0.2 0.3",
 )
 @click.option(
     "--noise-threshold",
     type=float,
     default=None,
-    help="Threshold for rejecting IMF modes containing noise",
+    help="Threshold for rejecting IMF modes containing noise. If omitted, no modes will be rejected",
 )
 @click.option(
-    "--overwrite", is_flag=False, help="Overwrite existing IMF files in output directory"
+    "--overwrite", is_flag=True, help="Overwrite existing IMF files in output directory"
 )
 def decompose(
-    input, output, signal, timecol, noise, noise_threshold, overwrite
+    files, output, timecol, noise, noise_threshold, overwrite
 ):
-    """Decompose input data into IMFs"""
-    # Load data
-    print(f"Loading data from {input}")
-    dfs = load_data_from_csvs(input, timecol)
+    """
+    Decompose input data into IMFs
 
-    assert signal in dfs, f"Column {signal} not found in input data"
+    FILES expects either a single .csv, a list of .csvs or a directory containing .csv files.
+    Files containing more than one timeseries will result in multiple separate output files.
+
+    Resulting files will be named <column_name>_imf_<noise>.csv
+
+    e.g.: if an input file contains columns 'a' and 'b', and noise values 0.1 and 0.2 are specified,
+    the output files will be: a_imf_0.1.csv, a_imf_0.2.csv, b_imf_0.1.csv, b_imf_0.2.csv
+
+    Timeseries will be decomposed for the full time range available. If this is not what you intend (e.g.: if
+    performing a hindcast where you are training against only part of your signal data, and will use the rest for
+    validation), you should ensure that the input files provided are contain the correct subset of data.
+    """
+    # Load data
+    print(f"Loading data from {', '.join([str(f) for f in files])}")
+    dfs = {}
+    for file in files:
+        loaded = load_data_from_csvs(file, timecol)
+        for key in loaded:
+            assert key not in dfs, f"Duplicate column {key} found in input from file {file}"
+        dfs.update(loaded)
+
     print(
         f'Found {len(dfs)} timeseries in input data, with columns: {", ".join(dfs.keys())}'
     )
 
+    # Output folder
+    if output is None:
+        output = pathlib.Path.cwd()
+    output.mkdir(parents=True, exist_ok=True)
+
+    # Check if files are in output directory
+    if any([output == file.parent for file in files if file.is_file()]) \
+            or any([output == file for file in files if file.is_dir()]):
+        print("WARNING: Some or all input files are in the output directory, this may lead to confusing file names.")
+
     # Decompose each timeseries and save result
-    imf_dir = output / "imfs"
-    imf_dir.mkdir(parents=True, exist_ok=True)
     for col in tqdm(dfs, desc="Decomposing IMFs"):
         for ns in tqdm(noise, desc=f"Decomposing {col}", leave=False):
-            filename = imf_filename(imf_dir, col, ns)
+            filename = imf_filename(output, col, ns)
             if not overwrite and filename.exists():
+                tqdm.write(f"IMFs for {col} with noise {ns} already exist, skipping")
                 continue
             imf_dfs = steps.decompose(
                 dfs[col], noise=ns, num_trials=100, progress=False
@@ -118,6 +138,26 @@ def match(output, signal, reject_noise, noise_threshold, frequency_threshold):
             imfs_by_noise[noise], signal, frequency_threshold
         )
         nearest_freq.to_csv(output / f"frequencies_{noise}.csv")
+
+
+@cli.command()
+@click.option(
+    "-w",
+    "--working-dir",
+    type=click.Path(
+        exists=False, file_okay=False, dir_okay=True, path_type=pathlib.Path
+    ),
+    help="Working directory, defaults to current directory",
+)
+@click.option("-s", "--signal", type=str, help="Column name of signal to fit to")
+def fit(working_dir, signal):
+    """Fit IMFs to signal"""
+    if working_dir is None:
+        working_dir = pathlib.Path.cwd()
+
+    imfs = load_imfs(working_dir / "imfs")
+
+
 
 
 @cli.command()
